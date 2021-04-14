c0ed01076042a15c514f7c50c701eca07bcff563
pradeepdahima <pradeep.dahima@gmail.com>
2021-04-14 20:09:22 +0530

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 export default function App() {
   const imageRef = useRef(null);
-  const primaryImg = "/public/bw/Speaker-187.jpg";
-  const secondaryImg = "/public/Speaker-187.jpg";
+  const primaryImg = "../public/bw/Speaker-187.jpg";
+  const secondaryImg = "../public/Speaker-187.jpg";
   return (
     <>
       <img
