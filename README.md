0a7219fb16c129aa5ea84d0796048ba324d6a7d7
pradeepdahima <pradeep.dahima@gmail.com>
2021-04-14 20:07:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8hrk3o
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8hrk3o)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+import "./style.css";
+
+export default function App() {
+  const imageRef = useRef(null);
+  const primaryImg = "/public/bw/Speaker-187.jpg";
+  const secondaryImg = "/public/Speaker-187.jpg";
+  return (
+    <>
+      <img
+        onMouseOver={() => {
+          imageRef.current.src = secondaryImg;
+        }}
+        onMouseOut={() => {
+          imageRef.current.src = primaryImg;
+        }}
+        src={primaryImg}
+        ref={imageRef}
+      />
+    </>
+  );
+}
